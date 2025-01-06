e75183009c0abc90863d70656c4f916126872b04
w-reif <wlodzimierzreif@gmail.com>
2025-01-06 22:13:39 +0000

    added algo explanation

--- algorithms.py
+++ algorithms.py
@@ -9,17 +9,18 @@
 
 # LINEAR SEARCH
 # Time Complexity: O(n)
+# def linear_search(arr, target):
+#     for i in range(len(arr)):
+#         if arr[i] == target:
+#             return i
+#     return -1
 
-def linear_search(arr, target):
-    for i in range(len(arr)):
-        if arr[i] == target:
-            return i
-    return -1
 
 # ORDERED LINEAR SEARCH
 # Time Complexity: O(n)
 # List must be sorted beforehand
 # Implement a break if the current element is greater than the target
+
 
 # BINARY LINEAR SEARCH
 # Time Complexity: O(log n)
@@ -47,8 +48,15 @@ def linear_search(arr, target):
 
 
 # ###################
+# ###################
+# ###################
+
 # SIMPLE SORT
 # Big-O Notation: O(n^2)
+# For each element in the list check the first element becomes the 'min'
+# Iterate thourh the list. If any following is smaller it becomes the 'min'
+# After the last iteration of the inner loop the 'min' element is removed from the list
+
 # str_input = input('Enter a string: ')
 # min = 0
 # sorted_str = []
@@ -64,6 +72,12 @@ def linear_search(arr, target):
 
 # BUBBLE SORT
 # Big-O Notation: O(n^2) but faster than simple sort
+# For each element in the list less the last on(n - 1) we iterate through the list from index 0 to 
+# index n - 1 - outer loop index
+# If element is greater than the following they swap places
+# We set hasSwapped to True. It tells the algo to break if the outer run completes without swapping
+# That means it's sorted
+
 # str_input = input("Enter a string: ")
 # n = len(str_input)
 # sorted_str = []
@@ -74,41 +88,72 @@ def linear_search(arr, target):
 #         if str[j] > str[j + 1]:
 #             str[j], str[j + 1] = str[j + 1], str[j]
 #             hasSwapped = True
+#     if not hasSwapped:
+#         break
 # print(', '.join(str)) # joining th list
 
-# MERGE SORT
-def merge_sort(arr):
-    result = []
-    if len(arr) > 1:
-        mid = len(arr) // 2
-        left_half = arr[:mid]
-        right_half = arr[mid:]
-        merge_sort(left_half)
-        merge_sort(right_half)
-        
-        i, j, k, = 0
-        while i < len(left_half) and j < len(right_half):
-            if left_half[i] < right_half[j]:
-                arr[k] = left_half[i]
-                i += 1
-            else:
-                arr[k] = right_half[j]
-                j += 1
-            k += 1
-            
-        while i < len(left_half):
-            arr[k] = left_half[1]
-            i, k += 1
-            
-        while j < len(right_half):
-            arr[k] = right_half[j]
-            j, k += 1
-            
-input_string = input('Enter a string: ')
-split_input = input_string.split()
-print(split_input)
-print(type(split_input))
-# numbers = [int(n) for n in split_input]
 
-merge_sort(split_input)
-print(f'Sorted: {split_input}')
+# MERGE SORT
+# O(n log n)
+# Until list length is greated than 1 find a middle point. 
+# Split the array into two to and from the middle point(integer division)
+# while index is less than length of right and length of right list
+# if left_half[i] element is greated than right final_list[k] becomes that element. i++
+# if right or equal then right element becomes final_list[k]. j++
+# If 'i' is greater than len(left_half) final[k] = left[i]. i, k ++
+# If 'j' is greater than len(right_haf), final[k] = right[j]. j, k ++
+
+# def merge_sort(arr):
+#     result = []
+#     if len(arr) > 1:
+#         mid = len(arr) // 2
+#         left_half = arr[:mid]
+#         right_half = arr[mid:]
+#         merge_sort(left_half)
+#         merge_sort(right_half)
+
+#         i, j, k, = 0
+#         while i < len(left_half) and j < len(right_half):
+#             if left_half[i] < right_half[j]:
+#                 arr[k] = left_half[i]
+#                 i += 1
+#             else:
+#                 arr[k] = right_half[j]
+#                 j += 1
+#             k += 1
+
+#         while i < len(left_half):
+#             arr[k] = left_half[i]
+#             i, k += 1
+
+#         while j < len(right_half):
+#             arr[k] = right_half[j]
+#             j, k += 1
+
+# input_string = input('Enter a string: ')
+# split_input = input_string.split()
+# print(split_input)
+# print(type(split_input))
+# # numbers = [int(n) for n in split_input]
+
+# merge_sort(split_input)
+# print(f'Sorted: {split_input}')
+
+
+# INSERTION SORT
+# Big-O notation O(n^2)
+# Works best on partially sorted lists
+
+# For i in the list, j = i
+# while j > 0 and the previous element is greater than the current
+# current and previous swap places, j--
+
+def insertion_sort(input_list):
+    list = input_list
+    n = len(list)
+    for i in range(n):
+        j = i
+        while j > 0 and list[j - 1] > list[j]:
+            list[j], list[j - 1] = list[j - 1], list[j]
+            j -= 1
+    return list
--- sandbox.py
+++ sandbox.py
@@ -1,48 +1,14 @@
-# str_input = input("Enter a string: ")
-# n = len(str_input)
-# sorted_str = []
-# str = list(str_input)
-# hasSwapped = False
-# for i in range(n - 1):
-#     for j in range(n - 1 - i):
-#         if str[j] > str[j + 1]:
-#             str[j], str[j + 1] = str[j + 1], str[j]
-#             hasSwapped = True
-# print(', '.join(str)) # joining th list
+def insertion_sort(input_list):
+    list = input_list
+    n = len(list)
+    for i in range(n):
+        j = i
+        while j > 0 and list[j - 1] > list[j]:
+            list[j], list[j - 1] = list[j - 1], list[j]
+            j -= 1
+    return list
 
-def merge_sort(arr):
-    result = arr
-    if len(arr) > 1:
-        mid = len(arr) // 2
-        left_half = arr[:mid]
-        right_half = arr[mid:]
-        merge_sort(left_half)
-        merge_sort(right_half)
-        
-        i = j = k = 0
-        while i < len(left_half) and j < len(right_half):
-            if left_half[i] < right_half[j]:
-                result[k] = left_half[i]
-                i += 1
-            else:
-                result[k] = right_half[j]
-                j += 1
-            k += 1
-            
-        while i < len(left_half):
-            result[k] = left_half[i]
-            i += 1
-            k += 1
-            
-        while j < len(right_half):
-            result[k] = right_half[j]
-            j += 1
-            k += 1
-            
-    return result
-            
-input_string = input('Enter a string: ')
-split_input = list(input_string)
+print(insertion_sort(["3", "5", "9", "6", "8", "4", "3", "2", "1"]))
 
-sorted = merge_sort(split_input)
-print(f'Sorted: {sorted}')
+i = 2
+j = 1
